878f3c3fc48245f4ba9287c96ec1291aa3606be4
Paolo Moscoso <PaoloMoscoso@jalasoft.com>
2019-04-03 15:54:41 -0400

    Feature(backend): exercise_1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace CalculoSueldo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int horasTrabajadas;
+            float costoHora;
+            float sueldo;
+            string linea;
+            Console.Write("Ingrese Horas trabajadas por el operario:");
+            linea = Console.ReadLine();
+            horasTrabajadas = int.Parse(linea);
+            Console.Write("Ingrese el pago por hora:");
+            linea = Console.ReadLine();
+            costoHora = float.Parse(linea);
+            sueldo = horasTrabajadas * costoHora;
+            Console.Write("El sueldo total del operario es:");
+            Console.Write(sueldo);
+            Console.ReadKey();
         }
     }
 }
